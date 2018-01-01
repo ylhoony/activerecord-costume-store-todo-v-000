2a5f853d9e0d37cbb1845965009c2e9483b0c74b
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 08:14:33 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,6 +10,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
+
+      t.timestamps null: false
     end
   end
 end
