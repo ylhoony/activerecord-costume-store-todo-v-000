77dc8dd01cde668d0b1727f6ee21ffab8986e767
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 08:10:41 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.inetger :number_of_employees
+      t.integer :number_of_employees
       t.boolean :is_in_business
       t.string :opening_time
       t.string :closing_time
